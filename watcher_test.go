53d160eea05f385f7c072963576da9d52fd3e061
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-02-13 04:13:04 +0530

    tests: covers most usage patterns

--- watcher_test.go
+++ watcher_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/srivastavcodes/caskdb/utils"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -89,4 +90,83 @@ func TestWatch_Rotate_Insert_Scan(t *testing.T) {
 
 	val := q[idx][1]
 	require.Equal(t, val, event.Val)
+}
+
+func TestWatch_Put_Watch(t *testing.T) {
+	options := DefaultOptions
+	options.WatchQueueSize = 10
+	db, err := Open(options)
+	assert.NoError(t, err)
+	defer destroyDB(db)
+
+	w, err := db.Watch()
+	assert.NoError(t, err)
+	for i := 0; i < 50; i++ {
+		key := utils.GetTestKey(rand.Int())
+		value := utils.RandomBytes(128)
+		err = db.Put(key, value)
+		assert.NoError(t, err)
+		event := <-w
+		assert.Equal(t, WatchActionPut, event.Action)
+		assert.Equal(t, key, event.Key)
+		assert.Equal(t, value, event.Val)
+	}
+}
+
+func TestWatch_Put_Delete_Watch(t *testing.T) {
+	options := DefaultOptions
+	options.WatchQueueSize = 10
+	db, err := Open(options)
+	assert.NoError(t, err)
+	defer destroyDB(db)
+
+	w, err := db.Watch()
+	assert.NoError(t, err)
+
+	key := utils.GetTestKey(rand.Int())
+	value := utils.RandomBytes(128)
+	err = db.Put(key, value)
+	assert.NoError(t, err)
+	err = db.Delete(key)
+	assert.NoError(t, err)
+
+	for i := 0; i < 2; i++ {
+		event := <-w
+		assert.Equal(t, key, event.Key)
+		switch event.Action {
+		case WatchActionPut:
+			assert.Equal(t, value, event.Val)
+		case WatchActionDelete:
+			assert.Empty(t, event.Val)
+		}
+	}
+}
+
+func TestWatch_Batch_Put_Watch(t *testing.T) {
+	options := DefaultOptions
+	options.WatchQueueSize = 1000
+	db, err := Open(options)
+	require.NoError(t, err)
+	defer destroyDB(db)
+
+	w, err := db.Watch()
+	require.NoError(t, err)
+
+	times := 100
+	batch := db.NewBatch(DefaultBatchOptions)
+	for i := 0; i < times; i++ {
+		err = batch.Put(utils.GetTestKey(rand.Int()), utils.RandomBytes(128))
+		assert.NoError(t, err)
+	}
+	err = batch.Commit()
+	assert.NoError(t, err)
+
+	var batchId uint64
+	for i := 0; i < times; i++ {
+		event := <-w
+		if i == 0 {
+			batchId = event.BatchId
+		}
+		assert.Equal(t, batchId, event.BatchId)
+	}
 }
